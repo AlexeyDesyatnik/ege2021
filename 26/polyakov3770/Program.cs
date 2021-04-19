1e7cc225f7a9eec1b8e2d554682b6776ea8b6e43
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-04-19 21:39:40 +0300

    Сделал решение задачи 3770 на kpolyakov.spb.ru

---
+++ 26/polyakov3770/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace polyakov3770
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = System.IO.File
+                .ReadAllLines("26-53.txt")
+                .Skip(1)
+                .Select(line => Int32.Parse(line))
+                .ToList();
+            var numSet = new HashSet<int>(nums);
+            var count = 0;
+            var minAvg = 1000_000_000;
+            for (var i = 0; i < nums.Count; i++)
+                for (var j = i + 1; j < nums.Count; j++)
+                {
+                    if (nums[i] % 2 != 0 || nums[j] % 2 != 0) continue;
+                    var avg = (nums[i] + nums[j]) / 2;
+                    if (numSet.Contains(avg))
+                    {
+                        count++;
+                        if (minAvg > avg) minAvg = avg;
+                    }
+                }
+            Console.WriteLine(count);
+            Console.WriteLine(minAvg);
+        }
+    }
+}
