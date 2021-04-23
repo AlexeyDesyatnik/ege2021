e175f8cb29f0cba7891921854f6b691bd56e2d6c
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-04-23 08:42:19 +0300

    Добавил шаблон для решения зад.27 Статград вар.2010401 в C#

---
+++ 27/statgrad2010401_27_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace statgrad2010401_27_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
