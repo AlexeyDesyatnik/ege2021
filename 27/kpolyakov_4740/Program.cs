ed8f2f999c9c330f2cac7f6681e426872bacd697
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2022-02-05 12:05:37 +0300

    Added brute force C# solution for task 27-A (4740 on kpolyakov.spb.ru)

---
+++ 27/kpolyakov_4740/Program.cs
@@ -0,0 +1,33 @@
+﻿var lines = File.ReadAllLines("27-88a.txt");
+var n_k = lines[0].Split(' ');
+var n = int.Parse(n_k[0]);
+var k = int.Parse(n_k[1]);
+var a = lines[1..].Select(line => int.Parse(line)).ToArray();
+
+long maxsum = long.MinValue;
+for (int begin = 0; begin < a.Length; begin++)
+{
+    for (int end = begin + 1; end <= a.Length; end++)
+    {
+        long sum = 0;
+        int count = 0;
+        for (int i = begin; i < end; i++)
+        {
+            sum += a[i];
+            if (IsPrime(Math.Abs(a[i])) && a[i] < 0) count++;
+        }
+        if (count % k == 0) maxsum = Math.Max(maxsum, sum);
+    }
+}
+
+Console.WriteLine(maxsum);
+
+
+static bool IsPrime(int n)
+{
+    for (int d = 2; d * d <= n; d++)
+    {
+        if (n % d == 0) return false;
+    }
+    return true;
+}
